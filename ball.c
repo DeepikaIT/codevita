aee50451dda2dee07a5fef77d59bc5f58c4f707c
DeepikaIT <30291360+DeepikaIT@users.noreply.github.com>
2017-07-29 00:31:32 +0530

    Create ball.c

---
+++ ball.c
@@ -0,0 +1,60 @@
+#include<stdio.h>
+
+int solve(int N)
+ {
+ int L,R,U,D;
+        int a;
+       int k;
+       L=N/4;
+       R=N%4;
+        if(R==0)
+       return A%4;
+       else
+        {
+         a=0;
+              if(L%4)
+          {
+           for(k=1;k<R;++k)
+                 a=++k;
+                if(L==R)
+                return 1;
+          else
+          return 0;
+              }
+          else
+            {
+          for(k=0;k<R;++k)
+           a=++k;
+            if(L==R)
+               return 1;
+                else
+               return 0;
+        }
+        }
+        }
+
+        int main()
+        {
+        int T;
+        int N;
+         int result;
+         int i;
+        scanf("%d",&T);
+         if(T<1||T>100f(N<1||N>10000)
+      return 0;
+      if(N==0)
+      {
+          printf("/n-1");
+          
+      }
+      return 1;
+            result=solve(N);
+          if(result)
+             printf("Yes\n");
+        else
+           printf("\nno ball");
+          }
+     return 0;
+ }
+
+
