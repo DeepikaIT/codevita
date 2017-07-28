c08d120ff65cceca5204020625ebdb9db0e6920f
DeepikaIT <30291360+DeepikaIT@users.noreply.github.com>
2017-07-28 11:18:15 +0530

    Create vechileregisteration.c

---
+++ vechileregisteration.c
@@ -0,0 +1,32 @@
+#include&lt;stdio.h&gt;
+int main()
+
+{
+    int i,j=0,k,x;
+    int h=0;
+    scanf(&quot;%d&quot;,&amp;x);
+    if(x==01)
+    {
+    {
+    for(i=0;i&lt;26;i++)
+    {
+        for(j=1;j&lt;=9999;j++)
+            printf(&quot;TN 01 %c %04d\n&quot;,&#39;A&#39;+i,j);
+            h++;
+    }
+}
+for(i=65;i&lt;=90;i++)
+    {
+     for(j=65;j&lt;=90;j++)
+     {
+           for(k=1;k&lt;=9999;k++)
+{
+    printf(&quot;TN 01 %c%c %04d \n &quot;,i,j,k);
+    h++;
+    }}}}
+    else{
+        printf(&quot;enter the valid number&quot;);
+    }
+    printf(&quot;%d&quot;,h);
+    return 0;
+    }
